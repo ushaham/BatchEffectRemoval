59694643255d47842b6537f759e25bca72b0812d
uri <uri@euler.med.yale.edu>
2016-12-12 01:17:42 -0500

    copied MMD net from main script

--- src/CyTOF_calibration_noSkip_comparison.py
+++ src/CyTOF_calibration_noSkip_comparison.py
@@ -9,7 +9,7 @@ import keras.optimizers
 from Calibration_Util import DataHandler as dh 
 from Calibration_Util import FileIO as io
 from Calibration_Util import Misc
-from keras.layers import Input, Dense, merge, Activation
+from keras.layers import Input, Dense, merge, Activation, Dropout
 from keras.models import Model
 from keras import callbacks as cb
 import numpy as np
@@ -29,7 +29,6 @@ from statsmodels.distributions.empirical_distribution import ECDF
 from keras import initializations
 from numpy import genfromtxt
 import sklearn.preprocessing as prep
-from keras.callbacks import History 
 
 # configuration hyper parameters
 denoise = True # wether or not to train a denoising autoencoder to remover the zeros
@@ -43,16 +42,7 @@ l2_penalty_ae = 1e-2
 mmdNetLayerSizes = [25, 25]
 l2_penalty = 1e-2
 init = lambda shape, name:initializations.normal(shape, scale=.1e-4, name=name)
-init_ns = 'glorot_normal'
 
-# learning rate schedule
-def step_decay(epoch):
-    initial_lrate = 0.01
-    drop = 0.5
-    epochs_drop = 25.0
-    lrate = initial_lrate * math.pow(drop, math.floor((1+epoch)/epochs_drop))
-    return lrate
-lrate = LearningRateScheduler(step_decay)
 
 ######################
 ###### get data ######
@@ -93,12 +83,12 @@ if denoise:
 # rescale source to have zero mean and unit variance
 # apply same transformation to the target
 preprocessor = prep.StandardScaler().fit(source)
-source = preprocessor.transform(source)  
+source = preprocessor.transform(source) 
 target = preprocessor.transform(target)    
 
-###################################
-######## train MMD net       ######
-###################################
+#############################
+######## train MMD net ######
+#############################
 
 
 calibInput = Input(shape=(inputDim,))
@@ -119,19 +109,24 @@ block2_output = merge([block2_w2, block1_output], mode = 'sum')
 
 calibMMDNet = Model(input=calibInput, output=block2_output)
 
+# learning rate schedule
+def step_decay(epoch):
+    initial_lrate = 0.01
+    drop = 0.5
+    epochs_drop = 25.0
+    lrate = initial_lrate * math.pow(drop, math.floor((1+epoch)/epochs_drop))
+    return lrate
+lrate = LearningRateScheduler(step_decay)
 
 #train MMD net
-lrate = LearningRateScheduler(step_decay)
 optimizer = keras.optimizers.rmsprop(lr=0.0)
 
 calibMMDNet.compile(optimizer='rmsprop', loss=lambda y_true,y_pred: 
                cf.MMD(block2_output,target,MMDTargetValidation_split=0.1).KerasCost(y_true,y_pred))
 sourceLabels = np.zeros(source.shape[0])
-history = History()
 calibMMDNet.fit(source,sourceLabels,nb_epoch=500,batch_size=1000,validation_split=0.1,verbose=1,
-           callbacks=[lrate,mn.monitorMMD(source, target, calibMMDNet.predict),
-                      cb.EarlyStopping(monitor='val_loss',patience=50,mode='auto'), history])
-
+           callbacks=[mn.monitorMMD(source, target, calibMMDNet.predict),
+                      cb.EarlyStopping(monitor='val_loss',patience=50,mode='auto')])
 
 ####################################
 ######## train MMD net noSkip ######
