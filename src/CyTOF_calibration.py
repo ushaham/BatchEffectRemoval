fd5d1947456d20ec5c6ba6fe974a1392a9013cc1
ushaham <uri.shaham@yale.edu>
2016-12-11 22:59:30 -0500

    removed learning rate scheduler, increased l2 penalty parameters

--- src/CyTOF_calibration.py
+++ src/CyTOF_calibration.py
@@ -9,7 +9,7 @@ import keras.optimizers
 from Calibration_Util import DataHandler as dh 
 from Calibration_Util import FileIO as io
 from Calibration_Util import Misc
-from keras.layers import Input, Dense, merge, Activation
+from keras.layers import Input, Dense, merge, Activation, Dropout
 from keras.models import Model
 from keras import callbacks as cb
 import numpy as np
@@ -36,11 +36,11 @@ keepProb=.8
 
 # AE confiduration
 ae_encodingDim = 25
-l2_penalty_ae = 1e-3 
+l2_penalty_ae = 1e-2 
 
 #MMD net configuration
 mmdNetLayerSizes = [25, 25]
-l2_penalty = 5e-3
+l2_penalty = 1e-2
 init = lambda shape, name:initializations.normal(shape, scale=.1e-4, name=name)
 
 
@@ -121,11 +121,11 @@ lrate = LearningRateScheduler(step_decay)
 #train MMD net
 optimizer = keras.optimizers.rmsprop(lr=0.0)
 
-calibMMDNet.compile(optimizer=optimizer, loss=lambda y_true,y_pred: 
+calibMMDNet.compile(optimizer='rmsprop', loss=lambda y_true,y_pred: 
                cf.MMD(block2_output,target,MMDTargetValidation_split=0.1).KerasCost(y_true,y_pred))
 sourceLabels = np.zeros(source.shape[0])
 calibMMDNet.fit(source,sourceLabels,nb_epoch=500,batch_size=1000,validation_split=0.1,verbose=1,
-           callbacks=[lrate,mn.monitorMMD(source, target, calibMMDNet.predict),
+           callbacks=[mn.monitorMMD(source, target, calibMMDNet.predict),
                       cb.EarlyStopping(monitor='val_loss',patience=50,mode='auto')])
 
 ##############################
