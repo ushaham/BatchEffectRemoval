f427a0c087f318774d87fbe1cca17d1b98f2effa
ushaham <uri.shaham@yale.edu>
2016-12-14 15:19:08 -0500

    updated RNA script

--- src/Calibrate_RNA_example.py
+++ src/Calibrate_RNA_example.py
@@ -30,54 +30,30 @@ from keras import initializations
 
 # the input here is standardized PCs
 
-dataset = 'PC'
-lowRankApprox = False
-standardize = False
-
 #Define net configuration
 mmdNetLayerSizes = [50,50,50]
-#l2_penalty = 0e-3
-l2_penalty2 = 1e-2
-init = 'glorot_normal' 
-#init = lambda shape, name: initializations.normal(shape, scale=.1e-4, name=name)
-#dropoutProb = .0
-
-if dataset == 'raw':
-    patience = 50
-    epochsDrop = 25.0
-
-if dataset == 'PC':
-    patience = 50
-    epochsDrop = 25.0
+l2_penalty = 1e-2
+#my_init = 'glorot_normal' 
+def my_init (shape, name = None):
+    return initializations.normal(shape, scale=.1e-4, name=name)
     
 
 # learning rate schedule
 def step_decay(epoch):
-    initial_lrate = 0.01
+    initial_lrate = 0.001
     drop = 0.1
     epochs_drop = epochsDrop
     lrate = initial_lrate * math.pow(drop, math.floor((1+epoch)/epochs_drop))
     return lrate
 lrate = LearningRateScheduler(step_decay)
- 
-#dataPath = '/raid3/DropSeq/Retina/Second/Data2_standardized_37PCs.csv'
+
+patience = 50
+epochsDrop = 50.0
+
 dataPath = os.path.join(io.DeepLearningRoot(),'Data/Data2_standardized_37PCs.csv')
 data = genfromtxt(dataPath, delimiter=',', skip_header=0)
-#data = np.transpose(data)
-if lowRankApprox:
-    # obtain low rank approximation
-    U,d,V = np.linalg.svd(data, full_matrices = False)
-    D = np.diag(d)
-    rank = 37
-    data = np.dot(np.dot(U[:,:rank],D[:rank,:rank]),V[:rank,:])
-                    
-if standardize:    
-    preprocessor = prep.StandardScaler().fit(data)
-    data = preprocessor.transform(data)
-standardizedData = data
     
-#batchesPath = '/raid3/DropSeq/Retina/Second/batch_2rep.csv'
-batchesPath = '/raid3/DropSeq/Retina/Second/batch.csv'
+batchesPath = os.path.join(io.DeepLearningRoot(),'Data/batch.csv')
 batches = genfromtxt(batchesPath, delimiter=',', skip_header=0) 
 
 targetBatch = 1
@@ -101,29 +77,39 @@ source = data[batches == sourceBatch]
 calibInput = Input(shape=(space_dim,))
 block1_bn1 = BatchNormalization()(calibInput)
 block1_a1 = Activation('relu')(block1_bn1)
-block1_w1 = Dense(mmdNetLayerSizes[0], activation='linear',W_regularizer=l2(l2_penalty2), init = init)(block1_a1) 
+block1_w1 = Dense(mmdNetLayerSizes[0], activation='linear',W_regularizer=l2(l2_penalty), init = my_init)(block1_a1) 
 block1_bn2 = BatchNormalization()(block1_w1)
 block1_a2 = Activation('relu')(block1_bn2)
-block1_w2 = Dense(space_dim, activation='linear',W_regularizer=l2(l2_penalty2), init = init)(block1_a2) 
+block1_w2 = Dense(space_dim, activation='linear',W_regularizer=l2(l2_penalty), init = my_init)(block1_a2) 
 block1_output = merge([block1_w2, calibInput], mode = 'sum')
 block2_bn1 = BatchNormalization()(block1_output)
 block2_a1 = Activation('relu')(block2_bn1)
-block2_w1 = Dense(mmdNetLayerSizes[1], activation='linear',W_regularizer=l2(l2_penalty2), init = init)(block2_a1) 
+block2_w1 = Dense(mmdNetLayerSizes[1], activation='linear',W_regularizer=l2(l2_penalty), init = my_init)(block2_a1) 
 block2_bn2 = BatchNormalization()(block2_w1)
 block2_a2 = Activation('relu')(block2_bn2)
-block2_w2 = Dense(space_dim, activation='linear',W_regularizer=l2(l2_penalty2), init = init)(block2_a2) 
+block2_w2 = Dense(space_dim, activation='linear',W_regularizer=l2(l2_penalty), init = my_init)(block2_a2) 
 block2_output = merge([block2_w2, block1_output], mode = 'sum')
+block3_bn1 = BatchNormalization()(block2_output)
+block3_a1 = Activation('relu')(block3_bn1)
+block3_w1 = Dense(mmdNetLayerSizes[1], activation='linear',W_regularizer=l2(l2_penalty), init = my_init)(block3_a1) 
+block3_bn2 = BatchNormalization()(block3_w1)
+block3_a2 = Activation('relu')(block3_bn2)
+block3_w2 = Dense(space_dim, activation='linear',W_regularizer=l2(l2_penalty), init = my_init)(block3_a2) 
+block3_output = merge([block3_w2, block2_output], mode = 'sum')
 
-calibMMDNet = Model(input=calibInput, output=block2_output) 
+calibMMDNet = Model(input=calibInput, output=block3_output)
+
 
 #train MMD net
-optimizer = keras.optimizers.rmsprop(lr=0.0)    
+optimizer = keras.optimizers.rmsprop(lr=0.0)
+
 calibMMDNet.compile(optimizer=optimizer, loss=lambda y_true,y_pred: 
-               cf.MMD(block2_output,target,MMDTargetValidation_split=0.1).KerasCost(y_true,y_pred))    
-sourceLabels = np.zeros(source.shape[0]) # just because we need something
-calibMMDNet.fit(source,sourceLabels,nb_epoch=1000,batch_size=1000,validation_split=0.1,verbose=1,
-           callbacks=[lrate,mn.monitorMMD(source, target, calibMMDNet.predict),
-                      cb.EarlyStopping(monitor='val_loss',patience=patience,mode='auto')])
+               cf.MMD(block3_output,target,MMDTargetValidation_split=0.1).KerasCost(y_true,y_pred))
+sourceLabels = np.zeros(source.shape[0])
+calibMMDNet.fit(source,sourceLabels,nb_epoch=500,batch_size=1000,validation_split=0.1,verbose=1,
+           callbacks=[lrate, mn.monitorMMD(source, target, calibMMDNet.predict),
+                      cb.EarlyStopping(monitor='val_loss',patience=50,mode='auto')])
+
 afterCalib = calibMMDNet.predict(source)
 
 ##################################### qualitative evaluation: PCA #####################################
@@ -132,12 +118,9 @@ projection_after = pca.transform(afterCalib)
 # The PCs most correlated with the batch are 3 and 5
 pc1 = 3
 pc2 = 5
-if dataset =='PC':
-    sh.scatterHist(target[:,pc1], target[:,pc2], source[:,pc1], source[:,pc2])
-    sh.scatterHist(target[:,pc1], target[:,pc2], afterCalib[:,pc1], afterCalib[:,pc2])
-if dataset =='raw':
-    sh.scatterHist(target_sample_pca[:,pc1], target_sample_pca[:,pc2], projection_before[:,pc1], projection_before[:,pc2])
-    sh.scatterHist(target_sample_pca[:,pc1], target_sample_pca[:,pc2], projection_after[:,pc1], projection_after[:,pc2])
+
+sh.scatterHist(target[:,pc1], target[:,pc2], source[:,pc1], source[:,pc2])
+sh.scatterHist(target[:,pc1], target[:,pc2], afterCalib[:,pc1], afterCalib[:,pc2])
 
         
 ##################################### quantitative evaluation: MMD #####################################
