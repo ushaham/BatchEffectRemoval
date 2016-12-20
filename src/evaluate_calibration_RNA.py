95e982370f48d5bede2f29938648a727be711cad
uri <uri@euler.med.yale.edu>
2016-12-19 22:34:21 -0500

    updated results in RNA evaluation

--- src/evaluate_calibration_RNA.py
+++ src/evaluate_calibration_RNA.py
@@ -31,17 +31,17 @@ PCA_calibData = genfromtxt(pcaCalibDatapath, delimiter=',', skip_header=0)
 batchesPath = '/raid3/DropSeq/Retina/Second/1215/batch.csv'  
 batches = genfromtxt(batchesPath, delimiter=',', skip_header=0) 
 
-n_source = sum(batches == 2)
-n_target = sum(batches == 1)
+n_source = np.sum(batches == 2)
+n_target = np.sum(batches == 1)
 
-source = orgData(batches == 2)
-target = orgData(batches == 1)
-resNetCalibSource = MMD_calibData(batches == 2)
-CombatCalibSource = Combat_calibData(batches == 2)
-CombatCalibTarget = Combat_calibData(batches == 1)
-ZCalibSource = Z_calibData(batches == 2)
-PCA_CalibSource = PCA_calibData(batches == 2)
-PCA_CalibTarget = PCA_calibData(batches == 1)
+source = orgData[batches == 2]
+target = orgData[batches == 1]
+resNetCalibSource = MMD_calibData[batches == 2]
+CombatCalibSource = Combat_calibData[batches == 2]
+CombatCalibTarget = Combat_calibData[batches == 1]
+ZCalibSource = Z_calibData[batches == 2]
+PCA_CalibSource = PCA_calibData[batches == 2]
+PCA_CalibTarget = PCA_calibData[batches == 1]
 
 
 mmd_before = np.zeros(5)
@@ -53,7 +53,7 @@ mmd_target_target = np.zeros(5)
 
 for i in range(5):
     targetInds = np.random.randint(low=0, high = n_target, size = 1000)
-    sourceInds = np.random.randint(low=n_target, high = n_source + n_target, size = 1000)
+    sourceInds = np.random.randint(low=0, high = n_source, size = 1000)
     targetInds1 = np.random.randint(low=0, high = n_target, size = 1000)
     mmd_before[i] = K.eval(cf.MMD(source,target).cost(K.variable(value=source[sourceInds]), K.variable(value=target[targetInds])))
     mmd_after_resNet[i] = K.eval(cf.MMD(resNetCalibSource,target).cost(K.variable(value=resNetCalibSource[sourceInds]), K.variable(value=target[targetInds])))
@@ -70,4 +70,13 @@ print('MMD after calibration (Z):      ' + str(np.mean(mmd_after_Z))+'pm '+str(n
 print('MMD after calibration (PCA):    ' + str(np.mean(mmd_after_PCA))+'pm '+str(np.std(mmd_after_PCA)))
 print('MMD target-target:              ' + str(np.mean(mmd_target_target))+'pm '+str(np.std(mmd_target_target)))
 
+'''
+MMD before calibration:         0.436700809002pm 0.0047024403119
+MMD after calibration (resNet): 0.127874596417pm 0.00625510858653
+MMD after calibration (Combat): 0.158756631613pm 0.00749086873726
+MMD after calibration (Z):      0.256576797366pm 0.00532713034745
+MMD after calibration (PCA):    0.211824476719pm 0.010718020682
+MMD target-target:              0.114321789145pm 0.00166756124391
+
+'''
 
